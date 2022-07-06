b95825a7c265ad8c0c629f7f295fd9dbb51caa0f
foykon <foykon9@gmail.com>
2022-07-06 13:46:38 +0300

    06.07 kamp ödevi 4. gün 5.ödev

---
+++ MyDictonary/MyDictonary.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictonary
+{
+    class MyDictonary<T1,T2>
+    {
+        T1[] key;
+        T2[] value;
+        public MyDictonary()
+        {
+            key = new T1[0];
+            value = new T2[0];
+        }
+        public void Add(T1 newKey,T2 newValue)
+        {
+            T1[] tempKey = key;
+            T2[] tempValue = value;
+
+            key = new T1[key.Length+1];
+            value = new T2[value.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                key[i] = tempKey[i];
+                value[i] = tempValue[i];
+            }
+            key[key.Length - 1] = newKey;
+            value[value.Length - 1] = newValue;
+        }
+
+
+        public int Count
+        {
+            get { return key.Length; }
+        }
+
+
+        //tamamen kontrol amaçlı yazılmıştır içerdeki bilgilerin kayıt edilip edilmedini öğrenmek için
+        public void Show(MyDictonary<T1,T2> myDictonary)
+        {
+            for (int i = 0; i < myDictonary.Count; i++)
+            {
+                Console.WriteLine("key : " + myDictonary.key[i]+ "value : " + myDictonary.value[i]);
+            }
+            
+        }
+
+
+
+
+
+    }
+}
---
+++ MyDictonary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MyDictonary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictonary<int, int> myDictonary = new MyDictonary<int, int>();
+            myDictonary.Add(1, 1);
+            Console.WriteLine(myDictonary.Count);
+            myDictonary.Add(2, 2);
+            Console.WriteLine(myDictonary.Count);
+            myDictonary.Add(3, 3);
+            Console.WriteLine(myDictonary.Count);
+            myDictonary.Add(6, 6);
+            Console.WriteLine(myDictonary.Count);
+            myDictonary.Add(5, 5);
+            Console.WriteLine(myDictonary.Count);
+
+            myDictonary.Show(myDictonary);
+
+
+
+
+
+
+
+        }
+    }
+}
