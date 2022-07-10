5c7db683283ad94a615b5dde1a95948eafae80bb
foykon <foykon9@gmail.com>
2022-07-10 15:26:11 +0300

    10.07 5. ders bitti

---
+++ OOP2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Furkan";
+            musteri1.Soyadi = "Yıldız";
+            musteri1.TcNo = "12345678910";
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "54321";
+            musteri2.SirketAdi = "foyk";
+            musteri2.VergiNo = "951357";
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(musteri1);
+            customerManager.Add(musteri2);
+            customerManager.Add(musteri3);
+            customerManager.Add(musteri4);
+            
+
+
+
+        }
+    }
+}
